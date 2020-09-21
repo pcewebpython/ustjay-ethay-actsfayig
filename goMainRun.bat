009e9eb646dcb1be994f20dd475723693d18472d
mmanc125uw <mmanc125@uw.edu>
2020-09-21 11:19:45 -0400

    sp_py230 lesson5, setup and start use  request lib

---
+++ goMainRun.bat
@@ -0,0 +1,2 @@
+
+start python -u main.py
--- main.py
+++ main.py
@@ -6,20 +6,56 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+###
+###//hidden-journey-62459.herokuapp.com/piglatinize/
+### r = requests.post('https://httpbin.org/post', data = {'key':'value'})
+###
+#payload = {'key1': 'value1', 'key2': 'value2'}
+#>>> r = requests.post("https://httpbin.org/post", data=payload)
+#>>> print(r.text)
 
 def get_fact():
+
+    payload = {'input_text': 'test'}
+
+    #resource_url = "https://hidden-journey-62459.herokuapp.com/"
+    #r = requests.post(resource_url, allow_redirects=False, data=payload)
+    
+    resource_url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    r = requests.post(resource_url, allow_redirects=True, data=payload)
+
+    print(f"***MMM response text = -{r.text}-")
+
+    #soup = BeautifulSoup(response.content, "html.parser")
+    #facts = soup.find_all("div", id="content")
+
+    #print(f"***MMM facts = -{facts[0].getText()}-")
+    
+    return r.text
+
+    
+def get_fact_00():
 
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
+    print(f"***MMM facts = -{facts[0].getText()}-")
+    
     return facts[0].getText()
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+
+    print("***MMM aaaaaaaaaaaa")
+    result_fact = get_fact()
+    
+    #print(f"***MMM called result fact = -{facts[0].getText()}-")
+    
+    #return "FILL ME!"
+    return result_fact
 
 
 if __name__ == "__main__":
