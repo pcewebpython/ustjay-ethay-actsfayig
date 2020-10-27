11ffd205471f8d2ee138621adec83d6815323bba
Andy Simpson <asimps2006@gmail.com>
2020-10-27 12:27:13 -0700

    Added a pig_latinizer() function, added code to the home() function.  Now testing and de-linting. - AS

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import os
+#!/usr/bin/env python
+"""
+# ------------------------------------------------------------------------ #
+# Title: Lesson 5 - Mashups & Microservices.  This script demonstrates
+# how to scrape a random fact from http://unkno.com (Link to an external site)
+# and then send it to a pig latin web application running on Heroku.
+# Description: This is the main "main.py" script.  Use this command 'python -u main.py'
+# to start the server.
+# ChangeLog (Who,When,What):
+# ASimpson, 10/27/2020, Modified code to complete lesson5 - Assignment05
+# ------------------------------------------------------------------------ #
+"""
 
+import os
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
@@ -8,7 +20,7 @@ app = Flask(__name__)
 
 
 def get_fact():
-
+    """Get the ransom factoid from this website http://unkno.com"""
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -16,10 +28,22 @@ def get_fact():
 
     return facts[0].getText()
 
+def pig_latinizer(message):
+    '''pig latin address function generator'''
+    response = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/",
+                             data ={'input_text': message}, allow_redirects=False, stream=True)
+    pig_latin_message = response.headers['Location']
+    return pig_latin_message
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    """Take a random fact from one website, convert it to Pig Latin and then show the address"""
+    str_fact_message = str(get_fact())
+    str_pig_latin_addr = pig_latinizer(str_fact_message)
+    response_body = '<h1>Crazy Factoid:  <p style="font-size:18px;color:red"><i>"{0}"</i></p></h1>' \
+                    '<h3>Convert this to Pig Latin (addr) ===>  <b>{1}</h3><br>' \
+                    '<a href="{1}">Click here for Pig Latin version!!</a>'.format(str_fact_message, str_pig_latin_addr)
+    return response_body
 
 
 if __name__ == "__main__":
