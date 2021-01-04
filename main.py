c7b96ab835baca369c754b22a7715f24fd7da7ea
geekwriter2 <geekwriter@hotmail.com>
2021-01-03 17:38:24 -0800

    init commit

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
-import os
+"""a simple app to serve up pig latin"""
 
+import os
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
+FACT_URL = 'http://unkno.com'
+PIG_LATINIZER_URL = 'http://hidden-journey-62459.herokuapp.com/piglatinize'
 
 def get_fact():
+    """ get the pig latin """
 
-    response = requests.get("http://unkno.com")
-
+    response = requests.get(FACT_URL)
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
+    pig_latin_prhase = facts[0].getText().strip()
 
-    return facts[0].getText()
+    return pig_latin_prhase
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    """main functionality"""
+
+    my_fact = get_fact()
+    fact_dict = {'input_text': my_fact}
+    print(fact_dict)
+    pig_latin_response = requests.post(PIG_LATINIZER_URL,
+                                       allow_redirects=False,
+                                       data=fact_dict)
+    print(pig_latin_response.headers)
+    pig_latin_location = pig_latin_response.headers['Location']
+    print('test_' + pig_latin_location)
+    pig_latin_url = f'<a href="{pig_latin_location}">{pig_latin_location}</a>'
+    return render_template('pig_latin_translator.jinja2', pig_latin=pig_latin_url)
+
+
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port) # heroku
+    app.run(host='localhost', port=port) # local
 
